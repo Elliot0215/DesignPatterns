45b9c53bcca3864f09b4cc265b6a3223f7fd9d9c
Elliot0215 <elliot_hack@163.com>
2020-05-05 15:59:39 +0800

    translate

---
+++ com/wp/test/TranslateDemo.py
@@ -0,0 +1,36 @@
+#coding=utf-8
+
+import requests
+import json
+from abc import ABCMeta,abstractmethod
+
+class BaseInterface(metaclass=ABCMeta):
+    @abstractmethod
+    def interface_api(self,src):
+        pass
+
+class BaseTranslate(metaclass=ABCMeta):
+    def __init__(self,interface):
+        self.interface = interface
+
+    @abstractmethod
+    def translate(self,src):
+        pass
+
+class YouDaoInterface(BaseInterface):
+    def interface_api(self,src):
+        self.result = requests.get("http://fanyi.youdao.com/translate?&doctype=json&type=AUTO&i={}".format(src))
+
+    def parse_json(self):
+        return json.loads(self.result.text)['translateResult'][0][0]['tgt']
+
+
+class MyTranslate(BaseTranslate):
+    def translate(self,src):
+        self.interface.interface_api(src)
+        return self.interface.parse_json()
+
+yd = YouDaoInterface()
+t = MyTranslate(yd)
+print(t.translate('hello'))
+
